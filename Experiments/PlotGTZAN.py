ca18f4e0e7861127bae6f94d272627730d43d9d6
Berk Unver <berkunver03@gmail.com>
2024-12-01 23:19:28 -0500

    Added PCA clustering

--- Experiments/PlotGTZAN.py
+++ Experiments/PlotGTZAN.py
@@ -1,8 +1,10 @@
 import numpy as np
+import sklearn.decomposition
 import k_means_constrained
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas
+import torch
 
 
 #
@@ -18,68 +20,21 @@ import pandas
 #           -berk, November 15, 2024
 
 
-
 gtzan_genre_count = 10
 gtzan_songs_per_genre = 100
 
-def find_gtzan_genres_most_associated_with_features(frame: pandas.DataFrame, plot: matplotlib.axes.Axes):
-    # The only features we want to look at are the ones from the second index onwards.
-    # The first two columns are filename and length (which is constant). We can ignore these.
-    frame_feature_start_index = 2
-    
-    # The last column is the genre label, and there are 20 pairs of mfcc mean and variance.
-    # As far as I can tell these become less meaningful the higher your mfcc index is.
-    # So we will ignore the last 16, and because there are mean and variance for each one,
-    # this becomes 32.
-    frame_feature_end_index = frame.columns.size - (1 + 16*2)
-    data = frame.to_numpy()[:, frame_feature_start_index:frame_feature_end_index].astype(np.float32)
-    
-    feature_count = data.shape[1]
-    feature_genre_stdev = np.ndarray((feature_count, gtzan_genre_count), dtype=np.float32)
-    # A [gtzan_genre_count, feature_count] array. Contains the standard deviation of each feature per genre.
 
-    # ToDo: Figure out how to vectorize this
-    for genre_index in range(gtzan_genre_count):
-        start_index = genre_index * gtzan_songs_per_genre
-        end_index = start_index + gtzan_songs_per_genre - 1
-        for feature_index in range(feature_count):
-            genre_feature = data[start_index:end_index, feature_index]
-            # All the values of a specific feature per genre
-            stdev = np.std(genre_feature)
-            feature_genre_stdev[feature_index, genre_index] = stdev
+graph_point_size = 10
+graph_cluster_label_size = 100
 
-    genres_ranked_per_feature = np.argsort(feature_genre_stdev, axis=1)
-
-    genre_names = np.array([frame.at[i * gtzan_songs_per_genre, "label"] for i in range(gtzan_genre_count)])
-    feature_names = np.array(frame.columns[frame_feature_start_index:frame_feature_end_index])
-
-    cell_text = genre_names[genres_ranked_per_feature]
-
-    plot.set_title(f"Genres most strongly associated with a given feature (Strongest = leftmost)")
-    plot.axis("off") 
-    table = plot.table(cellText=cell_text, rowLabels=feature_names, loc="center")
-    table.auto_set_font_size(False)
-    table.set_fontsize(4)
-    table.scale(1, 0.3)
-
-
-
-
-def plot_gtzan():
-    frame = pandas.read_csv("Datasets/GTZAN/features_30_sec.csv")
-    
-    #
-    # Generate the plots that cluster based on spectral centroid and bpm.
-    #
-
-    try:
-        bpm = frame["tempo"]
-        spectral_centroid = frame["spectral_centroid_mean"]
-    except ValueError:
-        assert False
-
-    # No spectral flatness in the GTZAN pre-extracted features so we will avoid that for now.
-    
+def gtzan_bpm_and_specular_centroid(
+    bpm: np.array,
+    spectral_centroid: np.array,
+    genre_names: np.array,
+    genre_labels: np.array,
+    cluster_plot: matplotlib.axes.Axes,
+    normalized_plot: matplotlib.axes.Axes,
+    genre_plot: matplotlib.axes.Axes):
 
     clf = k_means_constrained.KMeansConstrained(
         n_clusters = gtzan_genre_count,
@@ -91,16 +46,11 @@ def plot_gtzan():
     cluster_data = np.stack((bpm, spectral_centroid), axis=-1)
     cluster_labels = clf.fit_predict(cluster_data)
     
-    _, ((cluster_plot, normalized_plot), (genre_plot, feature_stdev_plot)) = plt.subplots(ncols=2, nrows=2)
-    
-    point_size = 10
-    label_size = 100
-
     cluster_plot.set_title("Cluster plot")
     cluster_plot.set_xlabel("bpm")
     cluster_plot.set_ylabel("spectral centroid")
-    cluster_plot.scatter(bpm, spectral_centroid, c=cluster_labels, cmap="plasma", s=point_size)
-    cluster_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=label_size)
+    cluster_plot.scatter(bpm, spectral_centroid, c=cluster_labels, cmap="plasma", s=graph_point_size)
+    cluster_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=graph_cluster_label_size)
 
     
     # bpm and spectral centroids are in drastically different units.
@@ -123,8 +73,8 @@ def plot_gtzan():
     normalized_plot.set_xlabel("normalized bpm")
     normalized_plot.set_ylabel("normalized spectral centroid")
 
-    normalized_plot.scatter(normalized_bpm, normalized_spectral_centroid, c=normalized_labels, cmap="plasma", s=point_size)
-    normalized_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=label_size)
+    normalized_plot.scatter(normalized_bpm, normalized_spectral_centroid, c=normalized_labels, cmap="plasma", s=graph_point_size)
+    normalized_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=graph_cluster_label_size)
 
     
     #
@@ -138,14 +88,189 @@ def plot_gtzan():
     # It has 100 0s followed by  100 2s followed by 100 3s, and so on.
     # These are integer labels by genre.
     #           -berk, November 11, 2024
-    genre_labels = np.repeat(np.arange(0, gtzan_genre_count), gtzan_songs_per_genre)
 
     genre_plot.set_title("Genre plot")
     genre_plot.set_xlabel("bpm")
     genre_plot.set_ylabel("spectral centroid")
-    genre_plot.scatter(bpm, spectral_centroid, c=genre_labels, cmap="plasma", s=point_size)
+    genre_plot.scatter(bpm, spectral_centroid, c=genre_labels, cmap="plasma", s=graph_point_size)
+    colors = plt.colormaps['plasma'](np.linspace(0, 1, gtzan_genre_count))
+    patches = [matplotlib.patches.Patch(color=color) for color in colors]
+    genre_plot.legend(patches, genre_names)    
 
-    find_gtzan_genres_most_associated_with_features(frame, feature_stdev_plot)
+
+
+def gtzan_genres_most_associated_with_features(
+    data: np.array,
+    genre_names: np.array,
+    feature_names: np.array,
+    plot: matplotlib.axes.Axes):
+
+    # The last 40 features in the input data are 20 pairs of mfcc means and variances.
+    # As far as I can tell these become less meaningful the higher your mfcc index is.
+    # So we will ignore the last 16, and because there are mean and variance for each one,
+    # this becomes 32.
+    feature_end_index = data.shape[1] - 32
+    data = data[:, :feature_end_index]
+    feature_names = feature_names[:feature_end_index]
+
+
+    feature_count = data.shape[1]
+    feature_genre_stdev = np.ndarray((feature_count, gtzan_genre_count), dtype=np.float32)
+    # A [gtzan_genre_count, feature_count] array. Contains the standard deviation of each feature per genre.
+
+    # ToDo: Figure out how to vectorize this
+    for genre_index in range(gtzan_genre_count):
+        start_index = genre_index * gtzan_songs_per_genre
+        end_index = start_index + gtzan_songs_per_genre - 1
+        for feature_index in range(feature_count):
+            genre_feature = data[start_index:end_index, feature_index]
+            # All the values of a specific feature per genre
+            stdev = np.std(genre_feature)
+            feature_genre_stdev[feature_index, genre_index] = stdev
+
+    genres_ranked_per_feature = np.argsort(feature_genre_stdev, axis=1)
+
+    cell_text = genre_names[genres_ranked_per_feature]
+
+    plot.set_title(f"Genres most strongly associated with a given feature (Strongest = leftmost)")
+    plot.axis("off") 
+    table = plot.table(cellText=cell_text, rowLabels=feature_names, loc="center")
+    table.auto_set_font_size(False)
+    table.set_fontsize(4)
+    table.scale(1, 0.3)
+
+
+
+def gtzan_pca(
+    data: np.array,
+    genre_names: np.array,
+    feature_names: np.array,
+    genre_labels: np.array,
+    cluster_plot: matplotlib.axes.Axes,
+    genre_plot: matplotlib.axes.Axes):
+    
+    pca = sklearn.decomposition.PCA(n_components=2)
+
+    # TODO: Fix having to transpose, then untranspose it!
+    pca.fit(data.T)
+
+    clf = k_means_constrained.KMeansConstrained(
+        n_clusters = gtzan_genre_count,
+        size_min = int(gtzan_songs_per_genre * 2/3),
+        size_max = int(gtzan_songs_per_genre * 1.5),
+        random_state = 0
+    )
+
+    components = pca.components_.T
+    labels = clf.fit_predict(components)
+
+    components_x = components[:, 0]
+    components_y = components[:, 1]
+
+    cluster_plot.set_title("PCA cluster plot")
+    cluster_plot.set_xlabel("Component with the greatest variance")
+    cluster_plot.set_ylabel("Component with the second-greatest variance")
+
+    cluster_plot.scatter(components_x, components_y, c=labels, cmap="plasma", s=graph_point_size)
+    cluster_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=graph_cluster_label_size)
+
+    genre_plot.set_title("PCA genre plot")
+    genre_plot.set_xlabel("Component with the greatest variance")
+    genre_plot.set_ylabel("Component with the second-greatest variance")
+    genre_plot.scatter(components_x, components_y, c=genre_labels, cmap="plasma", s=graph_point_size)
+
+
+def gtzan_torch_pca(
+    data: np.array,
+    genre_names: np.array,
+    feature_names: np.array,
+    genre_labels: np.array,
+    cluster_plot: matplotlib.axes.Axes,
+    genre_plot: matplotlib.axes.Axes):
+    
+    tensor = torch.Tensor(data)
+    tensor = torch.nn.functional.normalize(tensor, dim=1)
+
+    (_, _, principal_components) = torch.pca_lowrank(tensor)
+    
+    components = torch.matmul(tensor, principal_components[:, :2]).numpy()
+
+    clf = k_means_constrained.KMeansConstrained(
+        n_clusters = gtzan_genre_count,
+        size_min = int(gtzan_songs_per_genre * 2/3),
+        size_max = int(gtzan_songs_per_genre * 1.5),
+        random_state = 0
+    )
+
+    labels = clf.fit_predict(components)
+
+    components_x = components[:, 0]
+    components_y = components[:, 1]
+
+    cluster_plot.set_title("PCA cluster plot")
+    cluster_plot.set_xlabel("Component with the greatest variance")
+    cluster_plot.set_ylabel("Component with the second-greatest variance")
+
+    cluster_plot.scatter(components_x, components_y, c=labels, cmap="plasma", s=graph_point_size)
+    cluster_plot.scatter(clf.cluster_centers_[:, 0], clf.cluster_centers_[:, 1], marker="X", s=graph_cluster_label_size)
+    genre_plot.set_title("PCA genre plot")
+    genre_plot.set_xlabel("Component with the greatest variance")
+    genre_plot.set_ylabel("Component with the second-greatest variance")
+    genre_plot.scatter(components_x, components_y, c=genre_labels, cmap="plasma", s=graph_point_size)
+
+    colors = plt.colormaps['plasma'](np.linspace(0, 1, gtzan_genre_count))
+    patches = [matplotlib.patches.Patch(color=color) for color in colors]
+    genre_plot.legend(patches, genre_names)    
+
+#     color = plt.colormaps['plasma'](np.linspace(0.15, 0.85, gtzan_genre_count))
+#     for i in range(gtzan_genre_count):
+#         color = colors[i]
+#         genre_name = genre_names[i]
+# 
+#     for color in colors:
+#     genre_plot.legend(ncols=gtzan_genre_count, bbox_to_anchor=(0, 1), loc="lower left", fontsize="small")
+# 
+
+
+
+def plot_gtzan():
+    frame = pandas.read_csv("Datasets/GTZAN/features_30_sec.csv")
+    
+    # The first two features are:
+    # The file name (which we don't need)
+    # The sample length (which is always the same).
+    # So we can ignore them.
+    data = frame.to_numpy()[:, 2:frame.columns.size-1].astype(np.float32)
+    feature_names = np.array(frame.columns[2:frame.columns.size-1])
+    genre_names = np.array([frame.at[i * gtzan_songs_per_genre, "label"] for i in range(gtzan_genre_count)])
+    genre_labels = np.repeat(np.arange(0, gtzan_genre_count), gtzan_songs_per_genre)
+
+
+    _, ((cluster_plot, normalized_plot), (genre_plot, feature_stdev_plot), (pca_cluster_plot, pca_genre_plot)) = plt.subplots(ncols=2, nrows=3)
+    
+
+    gtzan_bpm_and_specular_centroid(
+        frame["tempo"],
+        frame["spectral_centroid_mean"],
+        genre_names,
+        genre_labels,
+        cluster_plot,
+        normalized_plot,
+        genre_plot)
+
+    gtzan_genres_most_associated_with_features(
+        data,
+        genre_names,
+        feature_names,
+        feature_stdev_plot)
+
+    gtzan_torch_pca(
+        data, 
+        genre_names,
+        feature_names,
+        genre_labels,
+        pca_cluster_plot,
+        pca_genre_plot)
 
     plt.show()
 
